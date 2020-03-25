33690dc421345f2fae0c1da98e81b41303f71470
NikenASPrasetyo <nikenasprasetyo@gmail.com>
2020-03-25 13:22:34 +0800

    inputoutput hasil

--- InputOutput.java
+++ InputOutput.java
@@ -4,8 +4,22 @@ public class InputOutput{
  public static void main(String [] args){
   Scanner scanner = new Scanner(System.in);
   String nama;
+  float tugas,uts,uas;
+
   System.out.print("Masukkan nama anda: ");
   nama = scanner.nextLine();
-  System.out.println("Halo, !"+nama);
+  System.out.println("Halo, "+nama);
+
+  System.out.print("Masukkan nilai Tugas anda: ");
+  tugas = scanner.nextFloat();
+
+  System.out.print("Masukkan nilai UTS anda: ");
+  uts = scanner.nextFloat();
+
+  System.out.print("Masukkan nilai UAS anda: ");
+  uas = scanner.nextFloat();
+
+  float jumlah = (tugas+uts+uas)/3;
+  System.out.print("Total nilai rata rata anda: "+jumlah);
 }
 }
